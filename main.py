9e9997dc4f0b2a26d9c6b1ea284dd98b8c1fe078
azmibasri <azmibasri391@gmail.com>
2025-03-19 14:52:55 +0700

    mengubah dogma menjadi berbasis OOP

--- main.py
+++ main.py
@@ -5,78 +5,89 @@ import os
 from tkinter import ttk,messagebox
 from cryptography.fernet import Fernet
 
-def muat_kunci():
-    file_kunci = "kunci.key"
-    if os.path.exists(file_kunci):
-        with open(file_kunci,"rb") as file:
-            return file.read()
-    else:
-        kunci = Fernet.generate_key()
-        with open(file_kunci,"wb") as file:
-            file.write(kunci)
-        return kunci
+class App:
+    def __init__(self,root):
+        self.root = root
 
-def cek_password():
-    password = KotakPassword.get().encode()
-    if bcrypt.checkpw(password,MASTER_HASH):
-        messagebox.showinfo("Login Berhasil", "Selamat datang!")
-        root.withdraw()
-        root.utama()
-    else:
-        messagebox.showerror("Login Gagal?","Password salah! Coba lagi.")
+        self.lebar_layar = root.winfo_screenwidth()
+        self.tinggi_layar = root.winfo_screenmmheight()
 
-def muat_data(DATA_FILE):
-    if not os.path.exists(DATA_FILE):
-        data = []
-        return
+        self.lebar_jendela = 400
+        self.tinggi_jendela = 300
+
+        self.posisi_x = (self.lebar_layar - self.lebar_jendela) // 2
+        self.posisi_y = (self.tinggi_layar - self.tinggi_jendela) // 2
+
+        self.root.title("Inventory Tracking")
+        self.root.geometry(f"{self.lebar_jendela}x{self.tinggi_jendela}+{self.posisi_x}+{self.posisi_y+300}")
+        self.root.overrideredirect(True)
+        self.root.resizable(False,False)
+
+        self.MASTER_HASH = b'$2b$12$97EhEKjGzbWqEMDT11JWCuA0SpPPG5Eumx4rZy7VV9Gd8Sf8QUJTG'
+
+        
+        self.kunci = self.muat_kunci()
+        self.cipher = Fernet(self.kunci)
+        self.file_data = "kunci.json"
+        self.muat_data()
+
+        self.KotakUtama = ttk.Frame(root)
+        self.KotakUtama.pack(padx=10,pady=60)
+
+        ttk.Label(self.KotakUtama, text="Password:").grid(row=1, column=1, padx=5, pady=5)
+        self.KotakPassword = ttk.Entry(self.KotakUtama, show="*")
+        self.KotakPassword.grid(row=1, column=2, padx=5, pady=5)
+
+        ttk.Button(self.KotakUtama, text="Login", command=self.cek_password).grid(row=2, column=2, pady=10)
     
-    try:
-        with open(DATA_FILE, "rb") as file:
-            encrypted_data = file.read()
-            decrypted_data = cipher.decrypt(encrypted_data)
-            data = json.loads(decrypted_data)
-    except:
-        data = []
-def simpan_data(data):
-    data_enkripsi = cipher.encrypt(json.dumps(data).encode())
-    with open(file_data,"wb") as file:
-        file.write(data_enkripsi)
+    def muat_kunci(self):
+        file_kunci = "kunci.key"
+        if os.path.exists(file_kunci):
+            with open(file_kunci,"rb") as file:
+                return file.read()
+        else:
+            kunci = Fernet.generate_key()
+            with open(file_kunci,"wb") as file:
+                file.write(kunci)
+            return kunci
+    def cek_password(self):
+        password = self.KotakPassword.get().encode()
+        if bcrypt.checkpw(password,self.MASTER_HASH):
+            messagebox.showinfo("Login Berhasil", "Selamat datang!")
+            self.root.withdraw()
+            self.dashboard()
+        else:
+            messagebox.showerror("Login Gagal","Password salah! Coba lagi.")
 
-root = tk.Tk()
+    def muat_data(self):
+        if not os.path.exists(self.file_data):
+            self.data = []
+            return
+        
+        try:
+            with open(self.file_data, "rb") as file:
+                encrypted_data = file.read()
+                decrypted_data = self.cipher.decrypt(encrypted_data)
+                self.data = json.loads(decrypted_data)
+        except:
+            self.data = []
+    
+    def simpan_data(self):
+        data_enkripsi = self.cipher.encrypt(json.dumps(self.data).encode())
+        with open(self.file_data,"wb") as file:
+            file.write(data_enkripsi)
+    
+    def dashboard(self):
+        self.root = tk.Toplevel(self.root)
+        self.root.title("Dashboard")
+        self.root.geometry("400x300")
+        self.root.mainloop()
 
-lebar_layar = root.winfo_screenwidth()
-tinggi_layar = root.winfo_screenmmheight()
 
-lebar_jendela = 400
-tinggi_jendela = 300
 
-posisi_x = (lebar_layar - lebar_jendela) // 2
-posisi_y = (tinggi_layar - tinggi_jendela) // 2
 
-root.title("Inventory Tracking")
-root.geometry(f"{lebar_jendela}x{tinggi_jendela}+{posisi_x}+{posisi_y+300}")
-root.overrideredirect(True)
-root.resizable(False,False)
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
 
-MASTER_HASH = b'$2b$12$97EhEKjGzbWqEMDT11JWCuA0SpPPG5Eumx4rZy7VV9Gd8Sf8QUJTG'
-
-kunci = muat_kunci()
-cipher = Fernet(kunci)
-file_data = "kunci.json"
-muat_data(file_data)
-
-KotakUtama = ttk.Frame(root)
-KotakUtama.pack(padx=10,pady=60)
-
-ttk.Label(KotakUtama, text="Password:").grid(row=1, column=1, padx=5, pady=5)
-KotakPassword = ttk.Entry(KotakUtama, show="*")
-KotakPassword.grid(row=1, column=2, padx=5, pady=5)
-
-ttk.Button(KotakUtama, text="Login", command=cek_password).grid(row=2, column=2, pady=10)
-
-def utama():
-    utama = tk.Toplevel()
-    utama.geometry("500x300")
-    utama.mainloop()
-
-root.mainloop()
